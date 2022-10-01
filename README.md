d76fee580424eb9d9eea099f7476e5513c90612a
cjihkr <cjihkr@gmail.com>
2022-10-01 11:09:36 +0900

    remove particles.js & add menubar & prettier
    Co-authored-by: Fermion08 <Fermion08@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+# 2022-gsgifted-project
--- js/draw.js
+++ js/draw.js
@@ -1,19 +1,22 @@
 const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
-canvas.width=window.innerWidth;
-canvas.height=window.innerHeight;
-var x = window.innerWidth/4+250;
-var y = window.innerHeight/2;
-var d1 = false, d2 = false, d3 = false, d4 = false;
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+var x = window.innerWidth / 4 + 250;
+var y = window.innerHeight / 2;
+var d1 = false,
+  d2 = false,
+  d3 = false,
+  d4 = false;
 
 //#region basic
 
 ctx.beginPath();
-ctx.lineWidth=10;
-ctx.strokeStyle="white";
-ctx.lineCap="round";
+ctx.lineWidth = 10;
+ctx.strokeStyle = "white";
+ctx.lineCap = "round";
 ctx.moveTo(x, y);
-ctx.lineTo(x+500, y);
+ctx.lineTo(x + 500, y);
 ctx.stroke();
 
 //#endregion
@@ -21,23 +24,25 @@ ctx.stroke();
 //#region d-line
 
 function dlt(i, xn, yn) {
-  setTimeout(function() {
-    ctx.lineTo(xn, yn+i);
-    ctx.stroke(); 
+  setTimeout(function () {
+    ctx.lineTo(xn, yn + i);
+    ctx.stroke();
   }, 100);
 }
 
 function dl() {
-  if(d2 == false) {return;}
-  var xn = x+250;
-  var yn = y/4
+  if (d2 == false) {
+    return;
+  }
+  var xn = x + 250;
+  var yn = y / 4;
   ctx.beginPath();
-  ctx.lineWidth=10;
-  ctx.strokeStyle="lightgreen";
-  ctx.lineCap="round";
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "lightgreen";
+  ctx.lineCap = "round";
 
   ctx.moveTo(xn, yn);
-  for(var i = 0; i < 800; i++) {
+  for (var i = 0; i < 800; i++) {
     dlt(i, xn, yn);
   }
 }
@@ -47,14 +52,16 @@ function dl() {
 //#region d-dot
 
 function dd() {
-  if(d2==false) {return;}
+  if (d2 == false) {
+    return;
+  }
   ctx.beginPath();
-  ctx.lineWidth=10;
-  ctx.strokeStyle="white";
-  ctx.lineCap="round";
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "white";
+  ctx.lineCap = "round";
 
   ctx.moveTo(x, y);
-  d3=true;
+  d3 = true;
 }
 
 //#endregion
@@ -62,37 +69,43 @@ function dd() {
 //#region d-arc
 
 function dat1(i) {
-  setTimeout(function(){
+  setTimeout(function () {
     ctx.beginPath();
-    ctx.lineWidth=10;
-    ctx.strokeStyle="orange";
-    ctx.lineCap="round";
-    ctx.arc(x, y, 350, -Math.PI/3, i/1000-Math.PI/3);
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "orange";
+    ctx.lineCap = "round";
+    ctx.arc(x, y, 350, -Math.PI / 3, i / 1000 - Math.PI / 3);
     ctx.stroke();
   }, 100);
 }
 
 function dat2(i) {
-  setTimeout(function(){
+  setTimeout(function () {
     ctx.beginPath();
-    ctx.lineWidth=10;
-    ctx.strokeStyle="orange";
-    ctx.lineCap="round";
-    ctx.arc(x+500, y, 350, Math.PI - Math.PI/3, Math.PI - Math.PI/3 + i/1000);
+    ctx.lineWidth = 10;
+    ctx.strokeStyle = "orange";
+    ctx.lineCap = "round";
+    ctx.arc(
+      x + 500,
+      y,
+      350,
+      Math.PI - Math.PI / 3,
+      Math.PI - Math.PI / 3 + i / 1000
+    );
     ctx.stroke();
   }, 100);
 }
 
 function da() {
   // if(d1 == false) return;
-  for(var i = 0; i < 2000*Math.PI/3; i++) {
+  for (var i = 0; i < (2000 * Math.PI) / 3; i++) {
     dat1(i);
   }
-  setTimeout(function(){}, 100);
-  for(var i = 0; i < 2000*Math.PI/3; i++) {
+  setTimeout(function () {}, 100);
+  for (var i = 0; i < (2000 * Math.PI) / 3; i++) {
     dat2(i);
   }
-  d2=true;
+  d2 = true;
 }
 
 //#endregion
