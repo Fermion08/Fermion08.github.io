579d32b7ea8349bafdf114ce7248a7476ed8513d
Fermion08 <minzzangkkim@gmail.com>
2022-10-08 08:45:55 +0900

    svg anime

---
+++ js/svgAnimations.js
@@ -0,0 +1,14 @@
+anime({
+  targets: 'svg',
+  rotateZ: '1turn',
+  translateX: 250,
+  easing: 'easeInOutQuad'
+});
+
+anime({
+  targets: '#bar',
+  width: '100px',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop:true
+});
