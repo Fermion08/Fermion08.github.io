6a6ee411866f15bfc7f6ac641399a89e2a4b1423
Fermion08 <minzzangkkim@gmail.com>
2022-10-10 13:03:55 +0900

    fucking angle page

--- js/angleAnimation.js
+++ js/angleAnimation.js
@@ -1,5 +1,3 @@
-let point = "M 10 10 A 20 20 1 1 0 10 150";
-
 // anime({
 //   targets: "pth",
 //   d: point,
@@ -7,20 +5,349 @@ let point = "M 10 10 A 20 20 1 1 0 10 150";
 //   direction: "alternate",
 //   loop: true,
 // });
-let arc = document.querySelectorAll(".arc");
-document.querySelector('#drawArc').onclick = function(){
-  document.getElementById('arc1').style.visibility = "visible";
-  document.getElementById('arc2').style.visibility = "visible";
-  drawArc()};
-function drawArc() {
+
+
+//#region bool V
+let arc1Is = false;
+let dot1Is = false;
+let arc2Is = false;
+let dot2Is = false;
+let lineIs = false;
+let rSideIs = false;
+//#endregion
+
+
+//#region getting Es
+let arc1 = document.querySelectorAll(".arc1");
+let dot1 = document.querySelectorAll(".dot1");
+let arc2 = document.querySelectorAll(".arc2");
+let dot2 = document.querySelectorAll(".dot2");
+let line = document.querySelectorAll(".line");
+let grid = document.querySelectorAll(".grid");
+let rSide = document.querySelectorAll(".rSide");
+let speedwagon = document.getElementById("speedwagon");
+//#endregion
+
+
+//#region draw
+
+function drawArc1() {
+  speedwagon.innerText="선분AB의 양 끝점에서 호를 겹치도록 그린다."
   anime({
-  targets: arc,
-  strokeDashoffset: [445, 222.5],
+  targets: arc1,
+  strokeDashoffset: [630, 450],
   easing: 'easeInOutCubic',
   duration: 1000,
   loop: 1}
-  
   );
-
+  arc1Is = true;
 }
+
+function drawDot1() {
+  speedwagon.innerText="호가 겹친 부분에 점을 찍는다."
+  anime({
+  targets: dot1,
+  r: [5, 3],
+  opacity: [0, 1],
+  easing: 'easeInOutCirc',
+  duration: 1000,
+  loop: 1}
+  );
+  dot1Is = true;
+}
+
+function drawArc2() {
+  speedwagon.innerText="선분AB의 양 끝점에서 호를 겹치도록 그린다."
+  anime({
+  targets: arc2,
+  strokeDashoffset: [315, 200],
+  easing: 'easeInOutCubic',
+  duration: 1000,
+  loop: 1}
+  );
+  arc2Is = true;
+}
+
+function drawLine() {
+  speedwagon.innerText="점C와 점D를 연결하여 선을 긎는다."
+  anime({
+  targets: line,
+  opacity: [0, 1],
+  easing: 'easeInOutCirc',
+  duration: 0,
+  loop: 1});
+  anime({
+    targets: line,
+    y2: [0, 160],
+    easing: 'easeInOutCirc',
+    duration: 1000,
+    loop: 1}
+    );
+  lineIs = true;
+}
+
+function drawR() {
+  speedwagon.innerText=""
+  anime({
+  targets: rSide,
+  opacity: [0, 1],
+  easing: 'easeInOutCirc',
+  duration: 0,
+  loop: 1});
+  anime({
+    targets: rSide,
+    strokeDashoffset: [100, 0],
+    easing: 'easeInOutCirc',
+    duration: 1000,
+    loop: 1}
+    );
+  rSideIs = true;
+}
+
+function gridOn() {
+  anime({
+    targets: grid,
+    opacity: 1,
+    easing: 'easeInOutSine',
+    duration: 500,
+    loop: 1}
+    );
+}
+
+function gridOff() {
+  anime({
+    targets: grid,
+    opacity: 0,
+    easing: 'easeInOutSine',
+    duration: 500,
+    loop: 1}
+    );
+}
+
+//#endregion
+
+
+//#region undraw
+function undrawArc1() {
+  speedwagon.innerText=""
+  anime({
+  targets: arc1,
+  strokeDashoffset: [450, 630],
+  easing: 'easeInOutCubic',
+  duration: 1000,
+  loop: 1}
+  );
+  arc1Is = false;
+}
+
+function undrawDot1() {
+  speedwagon.innerText="선분AB의 양 끝점에서 호를 겹치도록 그린다."
+  anime({
+  targets: dot1,
+  r: [3, 5],
+  opacity: [1, 0],
+  easing: 'easeInOutCirc',
+  duration: 1000,
+  loop: 1}
+  );
+  dot1Is = false;
+}
+
+function undrawArc2() {
+  speedwagon.innerText=""
+  anime({
+  targets: arc2,
+  strokeDashoffset: [200, 315],
+  easing: 'easeInOutCubic',
+  duration: 1000,
+  loop: 1}
+  );
+  arc2Is = false;
+}
+
+function undrawDot2() {
+  speedwagon.innerText="선분AB의 양 끝점에서 호를 겹치도록 그린다."
+  anime({
+  targets: dot2,
+  r: [3, 5],
+  opacity: [1, 0],
+  easing: 'easeInOutCirc',
+  duration: 1000,
+  loop: 1}
+  );
+  dot1Is = false;
+}
+
+function undrawLine() {
+  speedwagon.innerText="호가 겹친 부분에 점을 찍는다."
+  anime({
+    targets: line,
+    y2: [160, 0],
+    opacity: [1, 0],
+    easing: 'easeInOutCirc',
+    duration: 1000,
+    loop: 1}
+    );
+  lineIs = false;
+}
+
+function undrawR() {
+  speedwagon.innerText="점C와 점D를 연결하여 선을 긎는다."
+  anime({
+    targets: rSide,
+    strokeDashoffset: [0, 100],
+    opacity: [1, 0],
+    easing: 'easeInOutCirc',
+    duration: 1000,
+    loop: 1}
+    );
+  rSideIs = false;
+}
+
+
+//#endregion
+
+
+//#region bLine
+/** bLineP1 */
+function bordicLine(tar) {
+  target = document.querySelectorAll(tar);
+  anime({
+    targets: target,
+    strokeWidth: [2, 4],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+    );
+  anime({
+    targets: target,
+    strokeWidth: [4, 2],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+  );
+}
+/** bLineP3 */
+function bordicLine(tar1, tar2, tar3) {
+  target = [document.querySelectorAll(tar1), document.querySelectorAll(tar2), document.querySelectorAll(tar3)];
+  anime({
+    targets: target,
+    strokeWidth: [2, 4],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+    );
+  anime({
+    targets: target,
+    strokeWidth: [4, 2],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+  );
+}
+/** bLineP4 */
+function bordicLine(tar1, tar2, tar3, tar4) {
+  target = [document.querySelectorAll(tar1), 
+    document.querySelectorAll(tar2), 
+    document.querySelectorAll(tar3), 
+    document.querySelectorAll(tar4)];
+  anime({
+    targets: target,
+    strokeWidth: [2, 4],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+    );
+  anime({
+    targets: target,
+    strokeWidth: [4, 2],
+    easing: 'easeInOutCubic',
+    duration: 500,
+    loop: 1}
+  );
+}
+//#endregion
+
+
+//#region onclickFuncs
+document.querySelector('#drawArc1').onclick = function(){
+  if(arc1Is == true) {
+    undrawArc1();
+    
+  } else {
+    document.getElementById('arc1').style.visibility = "visible";
+    // document.getElementById('arc2').style.visibility = "visible";
+    drawArc1();
+  }
+};
+
 // stroke-dashoffset: 0;
+
+document.querySelector('#drawDot1').onclick = function(){
+  if(dot1Is == true) {
+    undrawDot1();
+  } else {
+    if(arc1Is == false) {
+      return;
+    } else {
+      drawDot1();
+    }
+  }
+};
+
+document.querySelector('#drawArc2').onclick = function(){
+  if(arc2Is == true) {
+    undrawArc2();
+    
+  } else {
+    if(dot1Is == false) {
+      return;
+    } else {
+      document.getElementById('arc2').style.visibility = "visible";
+      // document.getElementById('arc2').style.visibility = "visible";
+      drawArc2();
+    }
+  }
+};
+
+document.querySelector('#drawLine').onclick = function(){
+  if(lineIs == true) {
+    undrawLine();
+  } else {
+    if(dot1Is == false) {
+      return;
+    } else {
+      drawLine();
+    }
+  }
+};
+
+  
+document.querySelector('#drawR').onclick = function(){
+  if(rSideIs == true) {
+    undrawR();
+  } else {
+    if(lineIs == false) {
+      return;
+    } else {
+      drawR();
+    }
+  }    
+  };
+
+// document.querySelector('.switch').onclick = function(){
+//   if(gridIs == true) {gridOff; return;}
+//   gridOn();};
+
+  document.querySelector('.switch').onclick = function(){
+    let gridIs = document.querySelector("#gridTF").checked;
+    if(gridIs == false) {
+      gridOff();
+      gridIs = true;
+    } else {
+      gridOn();
+      gridIs = false;
+    }
+  };
+//#endregion
+
